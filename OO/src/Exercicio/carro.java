c566b28660aec2a70b45effd454da574144b8e94
HallanaKeury <hallanasousa10@gmail.com>
2022-08-11 06:18:19 -0300

    update

---
+++ OO/src/Exercicio/carro.java
@@ -0,0 +1,34 @@
+package Exercicio;
+
+public class carro {
+
+    //atributos
+    String cor;
+    String modelo;
+    int CapacidadeTanque;
+
+    //construtor aqui
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.CapacidadeTanque = capacidadeTanque;
+    }
+
+
+
+}
